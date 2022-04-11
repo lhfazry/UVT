ca9a9acdcb0a4b2ff71a2ce519094bb95f822b39
Fazry <lhuqita.fazry@gmail.com>
2022-04-11 14:17:53 +0700

    Fixing

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ parser.add_argument("--attention_heads", type=int, default="16", help="Attention
 parser.add_argument("--intermediate_size", type=int, default="8192", help="Intermediate Size")
 parser.add_argument("--num_hidden_layers", type=int, default="16", help="Transfomer block size")
 parser.add_argument("--rm_branch", type=str, default="SD", help="RM Branch")
+parser.add_argument("--latent_dim", type=int, default="1024", help="Latent Dimension")
+parser.add_argument("--max_seq_length", type=int, default="128", help="Maximum sequence length")
 
 params = parser.parse_args()
 
@@ -18,6 +20,8 @@ if __name__ == '__main__':
     intermediate_size = params.intermediate_size
     num_hidden_layers = params.num_hidden_layers
     rm_branch = params.rm_branch
+    latent_dim = params.latent_dim
+    max_seq_length = params.max_seq_length
 
     Network.train(  dataset_path=dataset_path,  # path to the dataset folder containing the "Videos" foldes and "FileList.csv" file
                     num_epochs=5,               # number of epoch to train
@@ -27,9 +31,9 @@ if __name__ == '__main__':
                     run_test=False,             # run test loop after each epoch
                     lr = 1e-5,                  # learning rate
                     modelname="UVT_repeat_reg",         # name of the folder where weight files will be stored
-                    latent_dim=1024,            # embedding dimension
+                    latent_dim=latent_dim,            # embedding dimension
                     lr_step_period=3,           # number of epoch before dividing the learning rate by 10
-                    ds_max_length = 128,        # maximum number of frame during training
+                    ds_max_length = max_seq_length,        # maximum number of frame during training
                     ds_min_spacing = 10,        # minimum number of frame during training
                     DTmode = 'sample',          # data preprocessing method: 'repeat' (mirroring) / 'full' (entire video) / 'sample' (single heartbeat with random amounf of additional frames)
                     SDmode = 'reg',             # SD branch network type: reg (regression) or cla (classification)
